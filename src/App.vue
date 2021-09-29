196a3d631c0ff4e0bac27174ba74155f9c1a0714
Benjamin AZOULAY <benjamin.azoulay@protonmail.com>
2021-09-29 15:29:24 +0200

    Useless imports removed

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,11 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-
 export default {
   name: 'App',
   computed: {
-    ...mapState(['weather'])
   },
   methods: {
-    ...mapActions(['saveWeather'])
   }
 }
 </script>
